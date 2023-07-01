e1e440b84384e35eff990e8463b6c1abe9b6e53e
Colson <colsoncalifornication@gmail.com>
2023-07-01 08:49:18 +0545

    style primary button

--- src/Button.jsx
+++ src/Button.jsx
@@ -10,7 +10,7 @@ function Button({
   outline,
   rounded,
 }) {
-  return <button>{children}</button>;
+  return <button className="px-3 py-1.5 border border-blue-600 bg-blue-500 text-white">{children}</button>;
 }
 
 Button.propTypes = {
