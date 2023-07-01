6e37260f1e9ba228ed74d801f5e1ba59957bb7ce
Colson <colsoncalifornication@gmail.com>
2023-07-01 18:58:41 +0545

    create accordion component

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,34 @@
-import { GoBell, GoContainer, GoDatabase } from 'react-icons/go';
-import Button from './components/Button';
-
+import Accordion from './components/Accordion';
 
 function App() {
-  const handleClick = () => {
-    console.log('clicked!!');
-  }
+  const items = [
+    {
+      label: 'What technologies will be used in frontend in typeMuse?',
+      content:
+        'Originally I was planning to write in pure REACT/REDUX with Server Side Rendering. Now the entire typeMuse system is being written in NEXT.js with TypeScript and style components.',
+    },
+    {
+      label: 'What Backend technologies will be used in typeMuse?',
+      content:
+        'On the backend part, the entire system will use NEST.js for API development alongside with GraphQL on some instances.',
+    },
+    {
+      label: 'What database will be used in typeMuse?',
+      content:
+        'typeMuse will use PostgreSQL for data store along with Amazon S3.',
+    },
+    {
+      label: 'More on software architecture and technologies in typeMuse?',
+      content:
+        'typeMuse will be using microservices architecture with technologies like Redis, Jenkins, Docker, Kubernetes, WebSockets, NGINX, Apache Kafka and more depending upon infrastructure and scalability. Later some services will be written in Golang for high performance!',
+    },
+    {
+      label: 'When I get to play typeMuse?',
+      content: 'typeMuse will be launched earlier next year (2024)!',
+    },
+  ];
 
-  return (
-    <div>
-      <div>
-        <Button secondary rounded outline className="mb-5" onClick={handleClick}>
-          <GoBell />
-          Add to Cart
-        </Button>
-      </div>
-      <div>
-        <Button danger outline onMouseEnter={handleClick}>
-          <GoContainer />
-          Buy me a Coffee
-        </Button>
-      </div>
-      <div>
-        <Button warning onMouseLeave={handleClick}>
-          <GoDatabase />
-          Order Now
-        </Button>
-      </div>
-      <div>
-        <Button secondary rounded>
-          Subscribe to Prime
-        </Button>
-      </div>
-      <div>
-        <Button primary rounded outline>
-          Update
-        </Button>
-      </div>
-    </div>
-  );
+  return <Accordion item={items} />;
 }
 
 export default App;
---
+++ src/components/Accordion.jsx
@@ -0,0 +1,5 @@
+function Accordion({ items }) {
+  return <div />;
+}
+
+export default Accordion;
