d35404892b34cd436436e7fa457df0b58899ec3f
Colson <colsoncalifornication@gmail.com>
2023-07-02 16:46:05 +0545

    implement state and event handler

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -1,5 +1,22 @@
+import { useState } from 'react';
+
 function Dropdown({ options }) {
-  return <div>Dropdown...</div>;
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleClick = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const renderedOptions = options.map((option, index) => {
+    return <div>{option.label}</div>;
+  });
+
+  return (
+    <div>
+      <div onClick={handleClick}>Select...</div>
+      {isOpen && <div>{renderedOptions}</div>}
+    </div>
+  );
 }
 
 export default Dropdown;
