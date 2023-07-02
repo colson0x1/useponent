e6c66f014c0b92c5619c2ff8467865f6ee443e10
Colson <colsoncalifornication@gmail.com>
2023-07-02 08:47:43 +0545

    initialize state and implement conditional rendering

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -1,9 +1,16 @@
+import { useState } from 'react';
+
 function Accordion({ items }) {
-  const renderedItems = items.map((item) => {
+  const [expandedIndex, setExpandedIndex] = useState(0);
+
+  const renderedItems = items.map((item, index) => {
+    const isExpanded = index === expandedIndex;
+
+    console.log(isExpanded);
     return (
       <div key={item.id}>
         <div>{item.label}</div>
-        <div>{item.content}</div>
+        <div>{isExpanded && <div>{item.content}</div>}</div>
       </div>
     );
   });
