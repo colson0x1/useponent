678f943d250c7595e6fecc9e94ef79b92220125b
Colson <colsoncalifornication@gmail.com>
2023-07-02 19:47:31 +0545

    add styles

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { GoChevronDown } from 'react-icons/go';
 
 function Dropdown({ options, value, onChange }) {
   const [isOpen, setIsOpen] = useState(false);
@@ -13,13 +14,30 @@ function Dropdown({ options, value, onChange }) {
   };
 
   const renderedOptions = options.map((option) => {
-    return <div onClick={() => handleOptionClick(option)}>{option.label}</div>;
+    return (
+      <div
+        className="hover:bg-sky-100 rounded cursor-pointer p-1"
+        onClick={() => handleOptionClick(option)}
+      >
+        {option.label}
+      </div>
+    );
   });
 
   return (
-    <div>
-      <div onClick={handleClick}>{value?.label || 'Select...'}</div>
-      {isOpen && <div>{renderedOptions}</div>}
+    <div className="w-48 relative">
+      <div
+        className="flex justify-between items-center cursor-pointer border rounded p-3 shadow bg-white w-full"
+        onClick={handleClick}
+      >
+        {value?.label || 'Select...'}
+        <GoChevronDown className="text-lg" />
+      </div>
+      {isOpen && (
+        <div className="absolute top-full border rounded p-3 shadow bg-white w-full">
+          {renderedOptions}
+        </div>
+      )}
     </div>
   );
 }
