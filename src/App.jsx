8dca912e073614d6e4e4866655cc68160e7a689f
Colson <colsoncalifornication@gmail.com>
2023-07-03 16:36:53 +0545

    implement Route Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import Link from './components/Link';
+import Route from './components/Route';
+import AccordionPage from './pages/AccordionPage';
+import DropdownPage from './pages/DropdownPage';
 
 function App() {
   return (
     <div>
       <Link to="/accordion">Go to accordion</Link>
-      <Link to="dropdown">Go to dropdown</Link>
+      <Link to="/dropdown">Go to dropdown</Link>
+      <div>
+        <Route path="/accordion">
+          <AccordionPage />
+        </Route>
+        <Route path="/dropdown">
+          <DropdownPage />
+        </Route>
+      </div>
     </div>
   );
 }
