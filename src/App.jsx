8311b464854716c78c160390a7c5a176da670827
Colson <colsoncalifornication@gmail.com>
2023-07-03 16:16:16 +0545

    implement Link Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
-import DropdownPage from './pages/DropdownPage';
+import Link from './components/Link';
 
-function App() {}
+function App() {
+  return (
+    <div>
+      <Link to="/accordion">Go to accordion</Link>
+      <Link to="dropdown">Go to dropdown</Link>
+    </div>
+  );
+}
 
 export default App;
