7700acef59043ac113eace94fceaf48883c36ab7
Colson <colsoncalifornication@gmail.com>
2023-07-03 19:48:21 +0545

    add styles to modal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,5 +1,10 @@
 function Modal() {
-  return <div>Modal</div>;
+  return (
+    <div>
+      <div className="absolute inset-0 bg-gray-300 opacity-80"></div>
+      <div className="absolute inset-40 p-10 bg-white">Modal</div>
+    </div>
+  );
 }
 
 export default Modal;
