e53b24292308617e00eaa483fe885d58ce385cdb
Colson <colsoncalifornication@gmail.com>
2023-07-03 15:45:18 +0545

    implement programmatic navigation

--- src/context/navigation.js
+++ src/context/navigation.js
@@ -16,9 +16,13 @@ function NavigationProvider({ children }) {
     };
   }, []);
 
+  const navigate = (to) => {
+    window.history.pushState({}, '', to);
+    setCurrentPath(to);
+  };
+
   return (
-    <NavigationContext.Provider value={{}}>
-      {currentPath}
+    <NavigationContext.Provider value={{ currentPath, navigate }}>
       {children}
     </NavigationContext.Provider>
   );
