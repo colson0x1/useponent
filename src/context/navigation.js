a0bca29ae259238e346c7bb96dcd1075873c2546
Colson <colsoncalifornication@gmail.com>
2023-07-03 15:15:09 +0545

    listen forward and back click

--- src/context/navigation.js
+++ src/context/navigation.js
@@ -1,10 +1,24 @@
-import { createContext } from 'react';
+import { createContext, useState, useEffect } from 'react';
 
 const NavigationContext = createContext();
 
 function NavigationProvider({ children }) {
+  const [currentPath, setCurrentPath] = useState(window.location.pathname);
+
+  useEffect(() => {
+    const handler = () => {
+      setCurrentPath(window.location.pathname);
+    };
+    window.addEventListener('popstate', handler);
+
+    return () => {
+      window.removeEventListener('popstate', handler);
+    };
+  }, []);
+
   return (
     <NavigationContext.Provider value={{}}>
+      {currentPath}
       {children}
     </NavigationContext.Provider>
   );
