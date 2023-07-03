8c856a4799c11095a3832d91f2cbe58be0c8a184
Colson <colsoncalifornication@gmail.com>
2023-07-03 17:06:26 +0545

    add custom hook

--- src/components/Link.jsx
+++ src/components/Link.jsx
@@ -1,9 +1,8 @@
 import classNames from 'classnames';
-import { useContext } from 'react';
-import NavigationContext from '../context/navigation';
+import useNavigation from '../hooks/use-navigation';
 
 function Link({ to, children }) {
-  const { navigate } = useContext(NavigationContext);
+  const { navigate } = useNavigation();
 
   const classes = classNames('text-blue-500');
 
--- src/components/Route.jsx
+++ src/components/Route.jsx
@@ -1,8 +1,7 @@
-import { useContext } from 'react';
-import NavigationContext from '../context/navigation';
+import useNavigation from '../hooks/use-navigation';
 
 function Route({ path, children }) {
-  const { currentPath } = useContext(NavigationContext);
+  const { currentPath } = useNavigation();
 
   if (path === currentPath) {
     return children;
