8454702da04c5e3e1d34f628fbbdb7d539c87e58
Colson <colsoncalifornication@gmail.com>
2023-07-04 07:42:26 +0545

    customize the modal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,14 +1,19 @@
 import ReactDOM from 'react-dom';
 
-function Modal({ onClose }) {
+function Modal({ onClose, children, actionBar }) {
   return ReactDOM.createPortal(
     <div>
-      <div onClick={onClose} className="absolute inset-0 bg-gray-300 opacity-80"></div>
-      <div className="absolute inset-40 p-10 bg-white">Modal</div>
-    </div>
-    ,
+      <div
+        onClick={onClose}
+        className="absolute inset-0 bg-gray-300 opacity-80"
+      ></div>
+      <div className="absolute inset-40 p-10 bg-white">
+        {children}
+        {actionBar}
+      </div>
+    </div>,
     document.querySelector('.modal-container')
-  )
+  );
 }
 
 export default Modal;
--- src/pages/ModalPage.jsx
+++ src/pages/ModalPage.jsx
@@ -13,12 +13,25 @@ function ModalPage() {
     setShowModal(false);
   };
 
+  const actionBar = (
+    <div>
+      <Button onClick={handleClose} primary>
+        Accept All
+      </Button>
+    </div>
+  );
+  const modal = (
+    <Modal onClose={handleClose} actionBar={actionBar}>
+      <p>We use cookies here at typeMuse to enhance your gaming experience.</p>
+    </Modal>
+  );
+
   return (
     <div>
       <Button onClick={handleClick} primary>
         Open Modal
       </Button>
-      {showModal && <Modal onClose={handleClose} />}
+      {showModal && modal}
     </div>
   );
 }
