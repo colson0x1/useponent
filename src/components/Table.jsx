1588caa4e21a87d41658cc6fde9906feb1d06f0f
Colson <colsoncalifornication@gmail.com>
2023-07-04 10:32:29 +0545

    implement dynamic table headers

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,29 +1,27 @@
-function Table({ data }) {
+function Table({ data, config }) {
+  const renderedHeaders = config.map((column) => {
+    return <th key={column.label}>{column.label}</th>;
+  });
+
   const renderedRows = data.map((user) => {
     return (
       <tr className="border-b" key={user.username}>
         <td className="p-3">@{user.username}</td>
         <td className="p-3">
-        <div className={`p-3 m-2 ${user.profileTheme}`}></div>
+          <div className={`p-3 m-2 ${user.profileTheme}`}></div>
         </td>
         <td className="p-3">{user.wpm} wpm</td>
       </tr>
-    )
-  })
+    );
+  });
   return (
     <table className="table-auto border-spacing-2">
-    <thead>
-      <tr className="border-b-2">
-      <th>Username</th>
-      <th>Profile Theme</th>
-      <th>Score</th>
-      </tr>
-    </thead>
-    <tbody>
-    {renderedRows}
-    </tbody>
+      <thead>
+        <tr className="border-b-2">{renderedHeaders}</tr>
+      </thead>
+      <tbody>{renderedRows}</tbody>
     </table>
-  )
+  );
 }
 
 export default Table;
--- src/pages/TablePage.jsx
+++ src/pages/TablePage.jsx
@@ -2,15 +2,21 @@ import Table from '../components/Table';
 
 function TablePage() {
   const data = [
-    { username: 'cols', profileTheme: 'bg-orange-500', wpm: 125 },
-    { username: 'nabineo', profileTheme: 'bg-yellow-500', wpm: 96 },
     { username: 'spark', profileTheme: 'bg-gray-500', wpm: 94 },
     { username: 'zephyr', profileTheme: 'bg-green-500', wpm: 81 },
+    { username: 'nabineo', profileTheme: 'bg-yellow-500', wpm: 96 },
+    { username: 'cols', profileTheme: 'bg-orange-500', wpm: 125 },
+  ];
+
+  const config = [
+    { label: 'Username' },
+    { label: 'Profile Theme' },
+    { label: 'Score' },
   ];
 
   return (
     <div>
-      <Table data={data} />
+      <Table data={data} config={config} />
     </div>
   );
 }
