95571cbed798c99aabf68b9b3bba1efb4da1f6cc
Colson <colsoncalifornication@gmail.com>
2023-07-04 08:15:40 +0545

    use useEffect to handle scroll overflow

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,13 +1,22 @@
 import ReactDOM from 'react-dom';
+import { useEffect } from 'react';
 
 function Modal({ onClose, children, actionBar }) {
+  useEffect(() => {
+    document.body.classList.add('overflow-hidden');
+
+    return () => {
+      document.body.classList.remove('overflow-hidden');
+    };
+  }, []);
+
   return ReactDOM.createPortal(
     <div>
       <div
         onClick={onClose}
-        className="absolute inset-0 bg-gray-300 opacity-80"
+        className="fixed inset-0 bg-gray-300 opacity-80"
       ></div>
-      <div className="absolute inset-40 p-10 bg-white">
+      <div className="fixed inset-40 p-10 bg-white">
         <div className="flex flex-col justify-between h-full text-blue-400 text-lg">
           {children}
           <div className="flex justify-end">{actionBar}</div>
