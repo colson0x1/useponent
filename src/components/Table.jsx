c10d6673e411d6f87177225680830a7e1d8ade5b
Colson <colsoncalifornication@gmail.com>
2023-07-04 09:56:37 +0545

    render rows with map

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,5 +1,27 @@
 function Table({ data }) {
-  return <div>Users: {data.length}</div>;
+  const renderedRows = data.map((user) => {
+    return (
+      <tr key={user.username}>
+        <td>@{user.username}</td>
+        <td>{user.profileTheme}</td>
+        <td>{user.wpm} wpm</td>
+      </tr>
+    )
+  })
+  return (
+    <table>
+    <thead>
+      <tr>
+      <th>Username</th>
+      <th>Profile Theme</th>
+      <th>Score</th>
+      </tr>
+    </thead>
+    <tbody>
+    {renderedRows}
+    </tbody>
+    </table>
+  )
 }
 
 export default Table;
--- src/pages/TablePage.jsx
+++ src/pages/TablePage.jsx
@@ -2,10 +2,10 @@ import Table from '../components/Table';
 
 function TablePage() {
   const data = [
-    { username: '@cols', color: 'bg-orange-500', wpm: 125 },
-    { username: '@nabineo', color: 'bg-yellow-500', wpm: 96 },
-    { username: '@spark', color: 'bg-red-500', wpm: 94 },
-    { username: '@zephyr', color: 'bg-green-500', wpm: 81 },
+    { username: 'cols', profileTheme: 'bg-orange-500', wpm: 125 },
+    { username: 'nabineo', profileTheme: 'bg-yellow-500', wpm: 96 },
+    { username: 'spark', profileTheme: 'bg-red-500', wpm: 94 },
+    { username: 'zephyr', profileTheme: 'bg-green-500', wpm: 81 },
   ];
 
   return (
