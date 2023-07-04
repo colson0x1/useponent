c92a7a85c4e4bc0c893df94532fece2dff932610
Colson <colsoncalifornication@gmail.com>
2023-07-04 07:24:49 +0545

    close the modal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,9 +1,9 @@
 import ReactDOM from 'react-dom';
 
-function Modal() {
+function Modal({ onClose }) {
   return ReactDOM.createPortal(
     <div>
-      <div className="absolute inset-0 bg-gray-300 opacity-80"></div>
+      <div onClick={onClose} className="absolute inset-0 bg-gray-300 opacity-80"></div>
       <div className="absolute inset-40 p-10 bg-white">Modal</div>
     </div>
     ,
--- src/pages/ModalPage.jsx
+++ src/pages/ModalPage.jsx
@@ -9,12 +9,16 @@ function ModalPage() {
     setShowModal(true);
   };
 
+  const handleClose = () => {
+    setShowModal(false);
+  };
+
   return (
     <div>
       <Button onClick={handleClick} primary>
         Open Modal
       </Button>
-      {showModal && <Modal />}
+      {showModal && <Modal onClose={handleClose} />}
     </div>
   );
 }
