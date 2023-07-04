b545c0d57acc988bc670af355a42ed85a7e8107d
Colson <colsoncalifornication@gmail.com>
2023-07-04 18:03:19 +0545

    add additional property that use computed value on the data

--- src/pages/TablePage.jsx
+++ src/pages/TablePage.jsx
@@ -1,33 +1,49 @@
-// import Table from '../components/Table';
 import SortableTable from '../components/SortableTable';
 
 function TablePage() {
+  function experiencePoint(races) {
+    const baseXP = 50;
+    const extraXPPerRaces = 10;
+    const totalXP = baseXP + (races - 1) * extraXPPerRaces;
+
+    return totalXP;
+  }
+
   const data = [
     {
       name: 'Shishir',
       username: 'spark',
       profileTheme: 'bg-yellow-500',
       wpm: 81,
+      races: 153,
     },
     {
       name: 'Samriddha',
       username: 'zephyr',
       profileTheme: 'bg-green-500',
       wpm: 72,
+      races: 171,
     },
     {
       name: 'Nabin',
       username: 'nabineo',
       profileTheme: 'bg-purple-500',
       wpm: 85,
+      races: 138,
     },
     {
       name: 'Colson',
       username: 'cols',
       profileTheme: 'bg-blue-500',
       wpm: 125,
+      races: 125,
     },
   ];
+
+  const updatedData = data.map((user) => {
+    const xp = experiencePoint(user.races);
+    return { ...user, xp: xp };
+  });
 
   const config = [
     {
@@ -48,6 +64,16 @@ function TablePage() {
       render: (user) => `${user.wpm} wpm`,
       sortValue: (user) => user.wpm,
     },
+    {
+      label: 'Races Today',
+      render: (user) => `${user.races} races`,
+      sortValue: (user) => user.races,
+    },
+    {
+      label: 'XP',
+      render: (user) => `${user.xp} xp`,
+      sortValue: (user) => user.xp,
+    },
   ];
 
   const keyFn = (user) => {
@@ -56,7 +82,7 @@ function TablePage() {
 
   return (
     <div>
-      <SortableTable data={data} config={config} keyFn={keyFn} />
+      <SortableTable data={updatedData} config={config} keyFn={keyFn} />
     </div>
   );
 }
