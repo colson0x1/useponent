d63c6b7a631b4f7a6bb937c6702fb2e3b47d4aea
Colson <colsoncalifornication@gmail.com>
2023-07-04 16:18:31 +0545

    implement sortOrder and sortBy states

--- src/components/SortableTable.jsx
+++ src/components/SortableTable.jsx
@@ -1,10 +1,22 @@
+import { useState } from 'react';
 import Table from './Table';
 
 function SortableTable(props) {
+  const [sortOrder, setSortOrder] = useState(null);
+  const [sortBy, setSortBy] = useState(null);
   const { config } = props;
 
   const handleClick = (label) => {
-    console.log(label);
+    if (sortOrder === null) {
+      setSortOrder('asc');
+      setSortBy(label);
+    } else if (sortOrder === 'asc') {
+      setSortOrder('desc');
+      setSortBy(label);
+    } else if (sortOrder === 'desc') {
+      setSortOrder(null);
+      setSortOrder(null);
+    }
   };
 
   const updatedConfig = config.map((column) => {
@@ -22,7 +34,12 @@ function SortableTable(props) {
     };
   });
 
-  return <Table {...props} config={updatedConfig} />;
+  return (
+    <div>
+      {sortOrder} - {sortBy}
+      <Table {...props} config={updatedConfig} />
+    </div>
+  );
 }
 
 export default SortableTable;
