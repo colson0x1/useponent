1eaa07d035ceca574057510262011c4daf2d6362
Colson <colsoncalifornication@gmail.com>
2023-07-05 11:05:42 +0545

    refractor Counter and add form and style it

--- src/pages/CounterPage.jsx
+++ src/pages/CounterPage.jsx
@@ -1,17 +1,54 @@
+import { useState } from 'react';
 import Button from '../components/Button';
-import useCounter from '../hooks/use-counter';
+import Panel from '../components/Panel';
 
 function CounterPage({ initialCount }) {
-  const { count, increment } = useCounter(initialCount);
+  const [count, setCount] = useState(initialCount);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  const decrement = () => {
+    setCount(count - 1);
+  };
 
   return (
-    <div>
+    <Panel className="m-3">
       <h1 className="text-3xl py-4 text-blue-500">Count is {count}</h1>
       <p className="text-lg py-4 text-blue-400">console.log({count})</p>
-      <Button primary rounded outline onClick={increment}>
-        Increment
-      </Button>
-    </div>
+      <div className="flex flex-row">
+        <Button
+          className="mr-2 my-4 text-red-400"
+          danger
+          outline
+          rounded
+          onClick={decrement}
+        >
+          Decrement
+        </Button>
+        <Button
+          className="mr-2 my-4"
+          rounded
+          primary
+          outline
+          onClick={increment}
+        >
+          Increment
+        </Button>
+      </div>
+
+      <form>
+        <label className="text-lg text-blue-400">Add a lot!</label>
+        <input
+          type="number"
+          className="p-1 m-3 bg-gray-50 border border-gray-300"
+        />
+        <Button className="" primary outline>
+          Add it!
+        </Button>
+      </form>
+    </Panel>
   );
 }
 
