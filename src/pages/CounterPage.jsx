e0a42b090de22d649ccc91b08cf74bad764e1748
Colson <colsoncalifornication@gmail.com>
2023-07-05 18:11:46 +0545

    refractor reducer with switch

--- src/pages/CounterPage.jsx
+++ src/pages/CounterPage.jsx
@@ -6,21 +6,20 @@ const INCREMENT_COUNT = 'increment';
 const SET_VALUE_TO_ADD = 'change_value_to_add';
 
 const reducer = (state, action) => {
-  if (action.type === INCREMENT_COUNT) {
-    return {
-      ...state,
-      count: state.count + 1,
-    };
+  switch (action.type) {
+    case INCREMENT_COUNT:
+      return {
+        ...state,
+        count: state.count + 1,
+      };
+    case SET_VALUE_TO_ADD:
+      return {
+        ...state,
+        valueToAdd: action.payload,
+      };
+    default:
+      return state;
   }
-
-  if (action.type === SET_VALUE_TO_ADD) {
-    return {
-      ...state,
-      valueToAdd: action.payload,
-    };
-  }
-
-  return state;
 };
 
 function CounterPage({ initialCount }) {
