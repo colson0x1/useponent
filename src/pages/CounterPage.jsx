bbe76de40982be421ebdc5e3ae46f56a0d649a0f
Colson <colsoncalifornication@gmail.com>
2023-07-05 08:28:42 +0545

    refactor and create custom hook for counter

--- src/pages/CounterPage.jsx
+++ src/pages/CounterPage.jsx
@@ -1,21 +1,16 @@
-import { useState, useEffect } from 'react';
 import Button from '../components/Button';
+import useCounter from '../hooks/use-counter';
 
 function CounterPage({ initialCount }) {
-  const [count, setCount] = useState(initialCount);
-
-  useEffect(() => {
-    console.log(count);
-  }, [count]);
-
-  const handleClick = () => {
-    setCount(count + 1);
-  };
+  const { count, increment } = useCounter(initialCount);
 
   return (
     <div>
-      <h1>Count is {count}</h1>
-      <Button onClick={handleClick}>Increment</Button>
+      <h1 className="text-3xl py-4 text-blue-500">Count is {count}</h1>
+      <p className="text-lg py-4 text-blue-400">console.log({count})</p>
+      <Button primary rounded outline onClick={increment}>
+        Increment
+      </Button>
     </div>
   );
 }
