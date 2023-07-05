778053db1fdc3dcf3b90c39751fd195264136488
Colson <colsoncalifornication@gmail.com>
2023-07-05 18:32:54 +0545

    add new state updates

--- src/pages/CounterPage.jsx
+++ src/pages/CounterPage.jsx
@@ -4,6 +4,8 @@ import Panel from '../components/Panel';
 
 const INCREMENT_COUNT = 'increment';
 const SET_VALUE_TO_ADD = 'change_value_to_add';
+const DECREMENT_COUNT = 'decrement';
+const ADD_VALUE_TO_COUNT = 'add_value_to_count';
 
 const reducer = (state, action) => {
   switch (action.type) {
@@ -11,6 +13,17 @@ const reducer = (state, action) => {
       return {
         ...state,
         count: state.count + 1,
+      };
+    case DECREMENT_COUNT:
+      return {
+        ...state,
+        count: state.count - 1,
+      };
+    case ADD_VALUE_TO_COUNT:
+      return {
+        ...state,
+        count: state.count + state.valueToAdd,
+        valueToAdd: 0,
       };
     case SET_VALUE_TO_ADD:
       return {
@@ -38,7 +51,9 @@ function CounterPage({ initialCount }) {
   };
 
   const decrement = () => {
-    // setCount(count - 1);
+    dispatch({
+      type: DECREMENT_COUNT,
+    });
   };
 
   const handleChange = (event) => {
@@ -53,8 +68,9 @@ function CounterPage({ initialCount }) {
   const handleSubmit = (event) => {
     event.preventDefault();
 
-    // setCount(count + valueToAdd);
-    // setValueToAdd(0);
+    dispatch({
+      type: ADD_VALUE_TO_COUNT,
+    });
   };
 
   return (
