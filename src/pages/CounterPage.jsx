2e1bddaab71529b2e0b2426e6c036cd8a73d1b12
Colson <colsoncalifornication@gmail.com>
2023-07-05 15:18:09 +0545

    implement Reducer

--- src/pages/CounterPage.jsx
+++ src/pages/CounterPage.jsx
@@ -1,35 +1,47 @@
-import { useState } from 'react';
+import { useReducer } from 'react';
 import Button from '../components/Button';
 import Panel from '../components/Panel';
 
+const reducer = (state, action) => {
+  return {
+    ...state,
+    count: state.count + 1,
+  };
+};
+
 function CounterPage({ initialCount }) {
-  const [count, setCount] = useState(initialCount);
-  const [valueToAdd, setValueToAdd] = useState(0);
+  // const [count, setCount] = useState(initialCount);
+  // const [valueToAdd, setValueToAdd] = useState(0);
+  const [state, dispatch] = useReducer(reducer, {
+    count: initialCount,
+    valueToAdd: 0,
+  });
+  console.log(state);
 
   const increment = () => {
-    setCount(count + 1);
+    dispatch();
   };
 
   const decrement = () => {
-    setCount(count - 1);
+    // setCount(count - 1);
   };
 
   const handleChange = (event) => {
     const value = parseInt(event.target.value) || 0;
 
-    setValueToAdd(value);
+    // setValueToAdd(value);
   };
 
   const handleSubmit = (event) => {
     event.preventDefault();
 
-    setCount(count + valueToAdd);
-    setValueToAdd(0);
+    // setCount(count + valueToAdd);
+    // setValueToAdd(0);
   };
 
   return (
     <Panel className="m-3">
-      <h1 className="text-3xl py-4 text-blue-500">Count is {count}</h1>
+      <h1 className="text-3xl py-4 text-blue-500">Count is {state.count}</h1>
       <div className="flex flex-row">
         <Button
           className="mr-2 my-4 text-red-400"
@@ -54,7 +66,7 @@ function CounterPage({ initialCount }) {
       <form onSubmit={handleSubmit}>
         <label className="text-lg text-blue-400">Add a lot!</label>
         <input
-          value={valueToAdd || ''}
+          value={state.valueToAdd || ''}
           onChange={handleChange}
           type="number"
           className="p-1 m-3 bg-gray-50 border border-gray-300"
